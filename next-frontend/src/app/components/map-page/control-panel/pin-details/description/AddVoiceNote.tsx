9cc21fe8d1381ea484c86b4ff2b4da284e868336
Rlee08 <rlee08@risd.edu>
2025-04-16 21:40:41 -0400

    update voice note recording and text input functionality

--- next-frontend/src/app/components/map-page/control-panel/pin-details/description/AddVoiceNote.tsx
+++ next-frontend/src/app/components/map-page/control-panel/pin-details/description/AddVoiceNote.tsx
@@ -18,13 +18,18 @@ export const AddVoiceNote = ({ onClose, setControlPanelState } : SelectLabelProp
     const [recording, setRecording] = useState<boolean>(false);
     const [audioURL, setAudioURL] = useState("");
     const [elapsedTime, setElapsedTime] = useState(0); // 초 단위 시간 저장
+    const [inputValue, setInputValue] = useState("Voice Note 1");
+    const [showInput, setShowInput] = useState(false);
+    const savedText = inputValue;
 
     const mediaRecorderRef = useRef<MediaRecorder | null>(null);
     const audioChunksRef = useRef<Blob[]>([]);
     const timerRef = useRef<number | undefined>(undefined);
-
+    
 
     const startRecording = async () => {
+        setElapsedTime(0); // 리셋 reset before start a new recording
+
         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
         mediaRecorderRef.current = new MediaRecorder(stream);
 
@@ -39,7 +44,6 @@ export const AddVoiceNote = ({ onClose, setControlPanelState } : SelectLabelProp
             const url = URL.createObjectURL(audioBlob);
             setAudioURL(url);
             audioChunksRef.current = [];
-            setElapsedTime(0); // 리셋
             clearInterval(timerRef.current);
         };
 
@@ -82,28 +86,62 @@ export const AddVoiceNote = ({ onClose, setControlPanelState } : SelectLabelProp
                     {/* Recording */}
                     <div className="flex flex-col gap-8">
                         {(recording) ? (
-                            //while recording
-                            <div className={"flex flex-col gap-4 items-center"}>
-                                <div className={"flex flex-col px-9 py-9 gap-5 border-light-purple border rounded-2xl "}>
-                                    <img src={"/logo/pause.svg"} alt={"pause logo"}/>
-                                    <div className={"flex gap-2 items-center"}>
-                                        <img src={"/logo/sound.svg"} alt={"sound logo"}/>
+                        // while recording
+                        <div className={"flex flex-col gap-4 items-center w-full"}>
+                            <div className={"w-full max-w-[500px] flex flex-col px-9 py-9 gap-5 border-light-purple border rounded-2xl"}>
+                                <button onClick={stopRecording} className="flex justify-center">
+                                    <img src={"/logo/pause.svg"} alt={"pause logo"} className="cursor-pointer w-20 h-20" />
+                                </button>
+                                <div className={"flex gap-2 items-center justify-center"}>
+                                    <img src={"/logo/sound.svg"} alt={"sound logo"} />
+                                    {formatTime(elapsedTime)}
+                                </div>
+                            </div>
+                        </div>
+
+                        ) : audioURL ? (
+                            // After recording finished
+                            <div className="flex flex-col gap-4 items-center w-full">
+                                <div className="w-full max-w-[500px] px-9 py-9 flex flex-col gap-5 border-light-purple border rounded-2xl">
+                                    <p className="text-center text-xl font-bold">Record Finished</p>
+                        
+                                    {/* Editable Title */}
+                                        <input
+                                            type="text"
+                                            value={inputValue}
+                                            onChange={(e) => setInputValue(e.target.value)}
+                                            onBlur={() => setShowInput(false)}
+                                            className="rounded-lg bg-white-10 px-3 py-1 text-center border border-gray-300"
+                                            autoFocus
+                                        />
+
+                                    {/* Duration */}
+                                    <div className="flex items-center justify-center gap-2">
+                                        <img src="/logo/sound.svg" className="w-5 h-5"></img>
                                         {formatTime(elapsedTime)}
                                     </div>
                                 </div>
-                                <SecondaryButton onClick={() => stopRecording()}>Stop</SecondaryButton>
+                        
+                                {/* Redo button */}
+                                <SecondaryButton onClick={() => startRecording()}>
+                                    <img src="/logo/redo_purple.svg" alt="redo icon" className="w-4 h-4 mr-2" />
+                                    Redo
+                                </SecondaryButton>
                             </div>
-                        ) : audioURL ? (
-                            // After recording finished
-                              <div className="flex flex-col gap-4 items-center w-full">
-                              <div className="w-full max-w-[500px] px-9 py-9 gap-5 border-light-purple border rounded-2xl">
-                                <p className="text-center text-sm font-bold">Record Finished</p>
-                                <audio controls src={audioURL} className="w-full" />
-                              </div>
-                            </div>
+
                         ) : (
-                            //before restarting
-                            <SecondaryButton onClick={() => startRecording()}>Start</SecondaryButton>
+                            //before recording
+                            <div className={"flex flex-col gap-4 items-center w-full"}>
+                                <div className={"w-full max-w-[500px] flex flex-col px-9 py-9 gap-5 border-light-purple border rounded-2xl"}>
+                                    <button onClick={startRecording} className="flex justify-center">
+                                        <img src={"/logo/microphone.svg"} alt={"start logo"} className="cursor-pointer w-20 h-20 transition-transform" />
+                                    </button>
+                                    <div className={"flex gap-2 items-center justify-center"}>
+                                        <img src={"/logo/sound.svg"} alt={"sound logo"} />
+                                        {formatTime(0)}
+                                    </div>
+                                </div>
+                            </div>
                     )}
                 </div>
 
